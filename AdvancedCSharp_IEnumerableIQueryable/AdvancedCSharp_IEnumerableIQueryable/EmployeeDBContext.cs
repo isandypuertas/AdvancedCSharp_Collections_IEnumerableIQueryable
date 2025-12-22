d787867955b5b09be7f742be38ee15ddf36e665b
isandypuertas <42593401+isandypuertas@users.noreply.github.com>
2025-12-22 20:01:37 +0100

    Add files via upload

---
+++ AdvancedCSharp_IEnumerableIQueryable/AdvancedCSharp_IEnumerableIQueryable/EmployeeDBContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedCSharp_IEnumerableIQueryable
+{
+    public class EmployeeDBContext : DbContext
+    {
+        private readonly ILoggerFactory loggerFactory = LoggerFactory.Create(config => config.AddConsole());
+        private readonly string connectionString;
+        public DbSet<Employee> Employees { get; set; }
+        public EmployeeDBContext(string connectionString)
+        {
+                this.connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            optionBuilder.UseLoggerFactory(loggerFactory);
+            optionBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
---
+++ AdvancedCSharp_IEnumerableIQueryable/AdvancedCSharp_IEnumerableIQueryable/Program.cs
@@ -0,0 +1,35 @@
+﻿
+namespace AdvancedCSharp_IEnumerableIQueryable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //this project needs packages: Microsoft.Extensions.Logging.Console, Microsoft.EntityFrameworkCore.SqlServer and Microsoft.EntityFrameworkCore
+            IEnumerableTest();
+            IQueryableTest();
+        }
+
+        private static void IEnumerableTest()
+        {
+            EmployeeDBContext context = new EmployeeDBContext("Server=NB-IPUERTAS\\SQL2016;\r\nDatabase=LocalDatabase;\r\nTrusted_Connection=True;\r\nTrustServerCertificate=True;\r\n");
+            IEnumerable<Employee> employees = context.Employees.Where(e => e.Id > 1);
+            employees = employees.Take(2);
+            foreach (Employee employee in employees)
+            {
+                Console.WriteLine($"Name:{employee.FirstName.Trim()} {employee.LastName.Trim()} Address:{employee.Address.Trim()}");
+            }
+        }
+
+        private static void IQueryableTest()
+        {
+            EmployeeDBContext context = new EmployeeDBContext("Server=NB-IPUERTAS\\SQL2016;\r\nDatabase=LocalDatabase;\r\nTrusted_Connection=True;\r\nTrustServerCertificate=True;\r\n");
+            IQueryable<Employee> employees = context.Employees.Where(e => e.Id > 1);
+            employees = employees.Take(2);
+            foreach (Employee employee in employees)
+            {
+                Console.WriteLine($"Name:{employee.FirstName.Trim()} {employee.LastName.Trim()} Address:{employee.Address.Trim()}");
+            }
+        }
+    }
+}
